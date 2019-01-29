7fecde7ee67d686f10f9e864be8a109e13586bf1
phuctrung0201 <mygalaxy2712@gmail.com>
2019-01-30 01:42:37 +0700

    Update index page

--- vendors/js/animate.js
+++ vendors/js/animate.js
@@ -18,3 +18,17 @@ document.addEventListener('aos:in:line-code', ({detail}) => {
         }
     }
   });
+  document.addEventListener('aos:in:achieved',function({detail}){
+      var x = 0;
+      var icrease = setInterval(increaseX,10);
+      function increaseX(){
+          if(x == 75){
+              clearInterval(icrease);
+          }else{
+              x=x+1;
+              $('#achieved').children('.progress').each(function(){
+                  $(this).children('.progress-bar').css('width',x +'%');
+              })
+          }
+      }x
+  })
