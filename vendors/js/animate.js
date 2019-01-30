a35212d9547088f6b9b0e56644b20decbf66d2d2
phuctrung0201 <mygalaxy2712@gmail.com>
2019-01-30 19:30:25 +0700

    Fixed reponsive

--- vendors/js/animate.js
+++ vendors/js/animate.js
@@ -1,42 +1,38 @@
+
 AOS.init({
     once: true,
     duration: 1000
 })
+AOS.refresh();
 
-document.addEventListener('aos:in:line-code', ({
-    detail
-}) => {
-    var x = 0;
-
-    var icrease = setInterval(increaseX, 1);
-
-    function increaseX() {
-
-        if (x == 2468) {
+document.addEventListener('aos:in:line-code', ({detail}) => {
+    var x=0;
+    
+    var icrease = setInterval(increaseX,1);
+    function increaseX(){
+        
+        if(x == 2468){
             clearInterval(icrease);
-        } else {
-            x = x + 4;
+        }else{
+            x=x+4;
             $(detail).children("h3").text(x);
         }
     }
-});
-document.addEventListener('aos:in:achieved', function ({
-    detail
-}) {
-    var x = 0;
-    var icrease = setInterval(increaseX, 10);
-
-    function increaseX() {
-        if (x == 75) {
-            clearInterval(icrease);
-        } else {
-            x = x + 1;
-            $('#achieved').children('.progress').each(function () {
-                $(this).children('.progress-bar').css('width', x + '%');
-            })
-        }
-    }
-});
+  });
+  document.addEventListener('aos:in:achieved',function({detail}){
+      var x = 0;
+      var icrease = setInterval(increaseX,10);
+      function increaseX(){
+          if(x == 75){
+              clearInterval(icrease);
+          }else{
+              x=x+1;
+              $('#achieved').children('.progress').each(function(){
+                  $(this).children('.progress-bar').css('width',x +'%');
+              })
+          }
+      }x
+  })
 $(document).ready(function () {
     var timeDelay = 0;
     $(".linecode").each(function(){
@@ -61,4 +57,4 @@ $(document).ready(function () {
             }
         }
     });
-})  
+})
