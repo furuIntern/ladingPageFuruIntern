56cc7823093cb454f2611047052cc99bebac4faf
phuctrung0201 <mygalaxy2712@gmail.com>
2019-02-01 13:26:35 +0700

    Add back to top

--- vendors/js/custom.js
+++ vendors/js/custom.js
@@ -33,4 +33,22 @@ $(document).ready(function () {
       }
     }
   })
+  $(window).scroll(function(){
+    $('#btb').toggleClass('d-none',$(window).scrollTop() <= 50);
+  })
+  //Back to top
+  $('#btb').click(function(){
+    var pos = $(window).scrollTop();
+
+    function back(){
+      if(pos <=0){
+        clearInterval(scroll)
+      }else{
+        pos = pos - 50;
+        $(window).scrollTop(pos);
+      }
+    }
+    var scroll = setInterval(back,1);
+    
+  })
 })
